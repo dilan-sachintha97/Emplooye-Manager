c2615d4448b2de4c6fd727d2c96ea07b0b448a88
dilan-sachintha97 <dil97an@gmail.com>
2022-11-22 13:22:29 +0530

    set path and create entity class

---
+++ src/main/java/com/abc/employeemanager/model/Employee.java
@@ -0,0 +1,29 @@
+package com.abc.employeemanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private long id;
+
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+}
